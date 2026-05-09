2742ea15d4631698ac6f085d51bc24008fa2efd4
shaikasma2700-cyber <shaikasma2700@gmail.com>
2026-05-09 17:30:57 +0530

    Create result.py

---
+++ result.py
@@ -0,0 +1,27 @@
+name = input("Enter student name: ")
+
+m1 = int(input("Enter marks for Subject 1: "))
+m2 = int(input("Enter marks for Subject 2: "))
+m3 = int(input("Enter marks for Subject 3: "))
+m4 = int(input("Enter marks for Subject 4: "))
+m5 = int(input("Enter marks for Subject 5: "))
+
+total = m1 + m2 + m3 + m4 + m5
+average = total / 5
+
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("\n--- Student Result ---")
+print("Name:", name)
+print("Total Marks:", total)
+print("Average:", average)
+print("Grade:", grade)
